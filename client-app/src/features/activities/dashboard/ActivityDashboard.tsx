b8131dfad1de6fa36dba63a430fe64c9872e0735
IT_SUPPCHAI <IT_SUPPCHAI@PROGRAMMER>
2021-11-04 12:55:58 +0700

    End of section 9

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,24 +3,25 @@ import { useEffect } from 'react';
 import { Grid } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { useStore } from '../../../app/stores/store';
+import ActivityFilters from './ActivityFilters';
 import ActivityList from './ActivityList';
 
 export default observer(function ActivityDashboard() {
-    const {activityStore} = useStore();
-    const {loadActivities,activityRegistry} = activityStore;
+    const { activityStore } = useStore();
+    const { loadActivities, activityRegistry } = activityStore;
 
     useEffect(() => {
-        if(activityRegistry.size === 0) loadActivities();
-    }, [activityRegistry.size,loadActivities])
-  
-    if (activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
+        if (activityRegistry.size === 0) loadActivities();
+    }, [activityRegistry.size, loadActivities])
+
+    if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
     return (
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList/>
+                <ActivityList />
             </Grid.Column>
             <Grid.Column width='6'>
-             <h2>Activity filters</h2>
+                <ActivityFilters />
             </Grid.Column>
         </Grid>
     )
