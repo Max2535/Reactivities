b5d58f2c2f381ddbc97302e12851d348b516abef
IT_SUPPCHAI <IT_SUPPCHAI@PROGRAMMER>
2021-11-18 15:17:48 +0700

    End of section 19

---
+++ API/SignalR/ChatHub.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Threading.Tasks;
+using Application.Comments;
+using MediatR;
+using Microsoft.AspNetCore.SignalR;
+
+namespace API.SignalR
+{
+    public class ChatHub : Hub
+    {
+        private readonly IMediator _mediator;
+        public ChatHub(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var httpContext = Context.GetHttpContext();
+            var activityId = httpContext.Request.Query["activityId"];
+            await Groups.AddToGroupAsync(Context.ConnectionId,activityId);
+            var result = await _mediator.Send(new List.Query{ActivityId = Guid.Parse(activityId)});
+            await Clients.Caller.SendAsync("LoadComments",result.Value);
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task SendComment(Create.Command command)
+        {
+            var comment = await _mediator.Send(command);
+
+            await Clients.Group(command.ActivityId.ToString())
+                .SendAsync("ReceiveComment", comment.Value);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+    }
+}
