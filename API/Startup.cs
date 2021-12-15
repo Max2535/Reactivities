c1f9013e6d032562420a93a8931e0fc54836c785
IT_SUPPCHAI <IT_SUPPCHAI@PROGRAMMER>
2021-12-15 13:18:36 +0700

    FB login complete

--- API/Startup.cs
+++ API/Startup.cs
@@ -64,12 +64,12 @@ namespace API
             app.UseXfo(opt => opt.Deny());
             app.UseCsp(opt => opt
                 .BlockAllMixedContent()
-                .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com"))
+                .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com","sha256-xxx","sha256-ttt"))
                 .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com","data:"))
                 .FormActions(s => s.Self())
                 .FrameAncestors(s => s.Self())
-                .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com"))
-                .ScriptSources(s => s.Self().CustomSources("sha256-kXwZFeDqzQYQxMANlJcsdedkJvek1q5ncjzFrCq4x+I="))
+                .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com","https://www.facebook.com","https://platform-lookaside.fbsbx.com"))
+                .ScriptSources(s => s.Self().CustomSources("sha256-zzz","https://connect.facebook.net","sha256-yyy"))
             );
 
             if (env.IsDevelopment())
